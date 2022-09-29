c2cd9800bcda4aeb6fa4724b94b08e9af52c004c
Akoruu.Dev <akoruu.dev@gmail.com>
2022-09-29 19:18:14 -0300

    FEAT [Products]: Create a map Component

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,6 @@ import GlobalStyle from "../style/globalStyle";
 import Main from "./Main";
 import Aside from "./Aside";
 
-import Products from "../services/Mock";
-
 export default function App() {
     return(
         <>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,13 @@
+import Products from "../services/Mock";
+
+function Product({ product }) {
+    console.log(product)
+}
+
 export default function Main() {
     return(
-        <></>
+        <>
+            {Products.map(product => <Product product={product} />)}
+        </>
     )
 }
