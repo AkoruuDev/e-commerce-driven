0d940d7feff675e51ca4bac71ccc038006945eae
Akoruu.Dev <akoruu.dev@gmail.com>
2022-09-29 19:25:46 -0300

    BAS [Product]: Create a product info base for App

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,22 @@ import Products from "../services/Mock";
 
 function Product({ product }) {
     console.log(product)
+    return (
+        <div>
+            <img src={product.imagem} />
+            <div>
+                <p>{product.nome}</p>
+                <p>{product.preco}</p>
+            </div>
+            <div>Comprar</div>
+        </div>
+    )
 }
 
 export default function Main() {
     return(
         <>
-            {Products.map(product => <Product product={product} />)}
+            {Products.map(product => <Product key={product.id} product={product} />)}
         </>
     )
 }
