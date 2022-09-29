c43153b412d91f11c5917d85643659539be7e97f
Akoruu.Dev <akoruu.dev@gmail.com>
2022-09-29 20:08:27 -0300

    STYLE [Shop]: Update style Aside using Styled components

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,16 +1,29 @@
+import styled from "styled-components"
+
 function Item({ item }) {
     console.log(item)
     return (
         <>
-        
+        oioi
         </>
     )
 }
 
 export default function Aside({ setShop, shop }) {
     return(
-        <>
+        <Container>
             {shop?.map(item => <Item key={item.id} item={item} />)}
-        </>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    width: 30vw;
+    height: 100vh;
+    background-color: #f8f8f8;
+    box-shadow: 0 10px 20px 0 grey;
+
+    position: fixed;
+    right: 0;
+    top: 0;
+`
