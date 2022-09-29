3be8d42c762b909681f27f2953cce250a9628b41
Akoruu.Dev <akoruu.dev@gmail.com>
2022-09-29 19:12:40 -0300

    BAS [Structure]: Create Main.js and Aside.js and import for App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import GlobalStyle from "../style/globalStyle";
+
+import Main from "./Main";
+import Aside from "./Aside";
+
 import Products from "../services/Mock";
 
 export default function App() {
