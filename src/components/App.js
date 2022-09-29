13be4ffd47bc580cbeb115a81b5abba8c59fc484
Akoruu.Dev <akoruu.dev@gmail.com>
2022-09-29 19:10:11 -0300

    STYLE [globalStyle]: Create and import for App.js the globalStyle.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
+import GlobalStyle from "../style/globalStyle";
 import Products from "../services/Mock";
 
 export default function App() {
     return(
-        <></>
+        <>
+            <GlobalStyle />
+        </>
     )
 }
